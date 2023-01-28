efa5eb6efb37d3b67efe3e5649a1a04a314568c0
arpesh28 <arpeshgadekar@gmail.com>
2023-01-28 18:15:58 +0530

    feat: user auth

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const workoutsRoutes = require("./routes/workouts");
+const userRoutes = require("./routes/user");
 
 //  express app
 const app = express();
@@ -15,13 +16,12 @@ app.use((req, res, next) => {
 
 //  routes
 app.use("/api/workouts", workoutsRoutes);
+app.use("/api/user", userRoutes);
 
 //  connect to db
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(
-    "mongodb+srv://arpesh28:arpesh123@bookstore.sbgmnyb.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => {
     //  listen for requests
     app.listen(process.env.PORT, () => {
